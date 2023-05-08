9b60a47a69ade084ea9c67c66ede0eb760c8264e
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-05-08 02:40:43 +0200

    Minor example of usage

--- TestScene.gd
+++ TestScene.gd
@@ -1,4 +1,30 @@
 extends Node2D
 
+var schema := Prav.dict({
+	"name": Prav.string(),
+	"age": Prav.int(),
+	"addr": Prav.dict({
+		"number": Prav.int(),
+		"street": Prav.string(),
+	})
+}, true)
+
 func _ready() -> void:
-	pass
+	var result := schema.parse({
+		"name": "John",
+		"age": 23,
+		"addr": {
+			"number": 14,
+			"street": "Maystreet",
+			"number2": 36,
+			"street2": "Maple",
+		},
+	})
+	
+	# True, with all the data passed
+	prints(result.ok, result.value)
+	
+	result = schema.parse(23)
+	
+	# False, error message
+	prints(result.ok, result.value)
