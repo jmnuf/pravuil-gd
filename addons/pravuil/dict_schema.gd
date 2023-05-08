2209dcaadf3cf393bb172369973a03cc78e5baed
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-05-08 02:20:50 +0200

    Base dict parsing

--- addons/pravuil/dict_schema.gd
+++ addons/pravuil/dict_schema.gd
@@ -6,7 +6,49 @@ var _exact: bool
 func _init(shape: Dictionary, exact: bool = false, nullable: bool = false).(TYPE_DICTIONARY, nullable) -> void:
 	for k in shape.keys():
 		var value = shape[k]
-		assert(value is Schema, "Dictionary schema shape values must all be Schemas")
+		assert(value is Schema, "Dictionary schema shape values must ALL be Schemas")
 	
 	_shape = shape
 	_exact = exact
+
+
+func parse(value, main_type_err_message: String = "", sub_type_err_message: String = "") -> Result:
+	var result := .parse(value, main_type_err_message)
+	if not result.ok:
+		return result
+	
+	if sub_type_err_message.empty():
+		sub_type_err_message = "Expected {expected} but got {received}"
+	
+	var dict: Dictionary = value
+	var val_keys = dict.keys()
+	var shape_keys = _shape.keys()
+	for k in val_keys:
+		if shape_keys.has(k):
+			continue
+		
+		result = Result.new(false, "Shape mismatch: Passed element is missing key `%s`" % str(k))
+		return result
+	
+	if _exact:
+		for k in shape_keys:
+			if val_keys.has(k):
+				continue
+			
+			result = Result.new(false, "Shape mismatch: Passed element has extra key `%s`" % str(k))
+			return result
+	
+	var val := {}
+	for k in shape_keys:
+		var sub_value = value[k]
+		var schema = _shape[k]
+		
+		result = schema.parse(sub_value, sub_type_err_message)
+		if result.ok:
+			val[k] = result.value
+			continue
+		
+		var err_message = "Invalid element type at key %s: %s" % [str(k), result.value]
+		return Result.new(false, err_message)
+	
+	return Result.new(true, val)
